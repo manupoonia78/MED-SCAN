82d30d070ac3b243dbca8cd7812b0459d3b5abc9
Manu <manupoonia78@gmail.com>
2020-09-24 14:57:20 +0530

    Delete test2.py

--- test2.py
+++
@@ -1,9 +0,0 @@
-import pytesseract
-import cv2
-
-
-if __name__ == "__main__":
-    img = cv2.imread('test.png')
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    result = pytesseract.image_to_data(img, lang='eng', nice=0, output_type=pytesseract.Output.DICT)
-    print(result)
